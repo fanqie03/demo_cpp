ae73803c2b55bb4a1b39670d26e6ecfd91a227b0
mengfu188 <1242733702@qq.com>
2019-11-01 10:39:08 +0800

    Add compare.cpp

--- face_ncnn/main.cpp
+++ face_ncnn/main.cpp
@@ -12,6 +12,7 @@
 
 #define FACE_DETECT_SIZEH   448
 using namespace std;
+Arcface arc;
 
 float distance(int p1x, int p1y, int p2x, int p2y) {
     int dx = p1x-p2x;
@@ -26,10 +27,10 @@ double getPos(FaceInfo faceInfo){
     double range =abs(le-n)*1.0/abs(re-le);
 
     float chinDirection = abs(1.0 * (p[1] - p[3]) / (le - re + 1e-8));
-    float noseDistance = abs(n-le) * 1.0 / abs(n-re);
+    float noseDistance = abs(n-le) * 1.0 / abs(n-re + 1e-8);
 //    float noseDistance = distance(p[4], p[5], p[0], p[1]) / distance(p[4], p[5], p[2], p[3]);
 //    return range;
-//    return noseDistance;
+    return noseDistance;
 
 
     if(le > n and re < n and n < lm and n > rm) return 1; //扭头
@@ -70,7 +71,7 @@ vector<float> detect_single_img(cv::Mat img) {
 //    costtime = et - st;
 ////    LOGD("face_exactfeature preprocess cost %fs\n", costtime / CLOCKS_PER_SEC);
 //    st = clock();
-//    vector<float>feature = g_arcFace.getFeature(det);
+    feature = arc.getFeature(det);
 //    et = clock();
 //    costtime = et - st;
 
@@ -84,7 +85,10 @@ int main(int args, char **argv) {
     string img_path = argv[1];
 
     cv::Mat img = cv::imread(img_path);
+    printf("%d %d \r\n", img.cols, img.rows);
+
     vector<float> my_feature = detect_single_img(img);
+    printf("%d\r\n", my_feature.size());
     cv::VideoCapture capture(0);
     int src_width = capture.get(cv::CAP_PROP_FRAME_WIDTH);
     int src_height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
@@ -114,12 +118,19 @@ int main(int args, char **argv) {
             cv::rectangle(resize_frame, cv::Point(face.x[0], face.y[0]), cv::Point(face.x[1], face.y[1]),
                           cv::Scalar(0, 255, 0));
             vector<float> feature = face_exactfeature(in, face);
+//            printf("%d\r\n", feature.size());
 //            cout << feature[0] << endl;
-//            float sim = face_calcSimilar(feature, my_feature);
+            float sim = face_calcSimilar(feature, my_feature);
 //            printf("sim is %f\r\n", sim);
             double pos = getPos(face);
-            cv::putText(resize_frame, to_string(pos), cv::Point(face.x[0], face.y[0]), cv::FONT_HERSHEY_SIMPLEX, 1,
+            cv::putText(resize_frame, to_string(sim), cv::Point(face.x[0], face.y[0]), cv::FONT_HERSHEY_SIMPLEX, 1,
                         cv::Scalar(0, 255, 0),1, 8,false);
+            for(int i=0; i < 10; i+=2){
+                int *landmark = face.landmark;
+                cv::circle(resize_frame, cv::Point(landmark[i], landmark[i+1]), 1 , cv::Scalar(0, 255, 0));
+            }
+
+
 //            cv::putText(resize_frame, to_string(sim), cv::Point(face.x[0], face.y[0]), cv::FONT_HERSHEY_SIMPLEX, 1,
 //                        cv::Scalar(0, 255, 0));
         }
